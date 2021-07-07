5cb0fcf5463181f01b700217e4303bd82122bb6f
Dorian Smalley <smalley2001@fb.com>
2021-07-07 18:58:59 -0400

    User gets a feed of the 20 most recent posts. Added FeedActivity files

---
+++ app/src/main/java/com/codepath/instagramclone/PostsAdapter.java
@@ -0,0 +1,78 @@
+package com.codepath.instagramclone;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Post> posts;
+
+    public PostsAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull PostsAdapter.ViewHolder holder, int position) {
+
+        //When RecyclerView has to show an item to a user it will call this function
+        //with the item's position and ViewHolder
+        Post post = posts.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvUsername;
+        private ImageView ivImage;
+        private TextView tvDescription;
+
+
+        public ViewHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
+            super(itemView);
+
+            //Find views and store references to them so later we can access those
+            //views when binding data
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+        }
+
+        public void bind(Post post) {
+            //Bind the post data to the view elements
+            tvDescription.setText(post.getDescription());
+            tvUsername.setText(post.getUser().getUsername());
+            ParseFile image = post.getImage();
+            if (image != null) {
+                Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+        }
+    }
+}
